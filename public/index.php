99afa63346b9976e7dcc3592acf37d6dd18e5d32
Agung Priambada <apriambada@yahoo.co.id>
2018-12-29 19:07:43 +0800

    Update index

--- public/index.php
+++ public/index.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 date_default_timezone_set('Asia/Makassar');
-
 // konstanta ROOT & DS
 define('ROOT', dirname(dirname(__FILE__)));
 define('DS', DIRECTORY_SEPARATOR);
@@ -11,11 +10,8 @@ $loader->add('application', __DIR__.'/../src/application/agung.php');*/
 
 // ================ file konfigurasi framework ================ //
 require_once (ROOT .DS. 'config' .DS. 'config.php');
-
 // ================== file utama framework ==================== //
 require_once (ROOT .DS. 'src/application' .DS. 'agung.php');
-
-
 // fungsi menampilkan error
 setReporting();
 // fungsi memanggil controller
